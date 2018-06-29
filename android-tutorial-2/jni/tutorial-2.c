7d9cb9ac58ed368e57bd3bc1a4a9ffbdd7b99f39
xray <hexulei@dyinnovations.com>
2018-06-29 18:43:23 +0800

    add rtsp code

--- android-tutorial-2/jni/tutorial-2.c
+++ android-tutorial-2/jni/tutorial-2.c
@@ -3,6 +3,7 @@
 #include <android/log.h>
 #include <gst/gst.h>
 #include <pthread.h>
+#include <gst/rtsp-server/rtsp-server.h>
 
 GST_DEBUG_CATEGORY_STATIC (debug_category);
 #define GST_CAT_DEFAULT debug_category
@@ -131,56 +132,169 @@ static void check_initialization_complete (CustomData *data) {
   }
 }
 
+typedef struct
+{
+    gboolean white;
+    GstClockTime timestamp;
+} MyContext;
+
+/* called when we need to give data to appsrc */
+static void
+need_data (GstElement * appsrc, guint unused, MyContext * ctx)
+{
+  GstBuffer *buffer;
+  guint size;
+  GstFlowReturn ret;
+
+  size = 385 * 288 * 2;
+
+  buffer = gst_buffer_new_allocate (NULL, size, NULL);
+
+  /* this makes the image black/white */
+  gst_buffer_memset (buffer, 0, ctx->white ? 0xff : 0x0, size);
+
+  ctx->white = !ctx->white;
+
+  /* increment the timestamp every 1/2 second */
+  GST_BUFFER_PTS (buffer) = ctx->timestamp;
+  GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 2);
+  ctx->timestamp += GST_BUFFER_DURATION (buffer);
+
+  g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
+}
+
+/* called when a new media pipeline is constructed. We can query the
+ * pipeline and configure our appsrc */
+static void
+media_configure (GstRTSPMediaFactory * factory, GstRTSPMedia * media,
+                 gpointer user_data)
+{
+  GstElement *element, *appsrc;
+  MyContext *ctx;
+
+  /* get the element used for providing the streams of the media */
+  element = gst_rtsp_media_get_element (media);
+
+  /* get our appsrc, we named it 'mysrc' with the name property */
+  appsrc = gst_bin_get_by_name_recurse_up (GST_BIN (element), "mysrc");
+
+  /* this instructs appsrc that we will be dealing with timed buffer */
+  gst_util_set_object_arg (G_OBJECT (appsrc), "format", "time");
+  /* configure the caps of the video */
+  g_object_set (G_OBJECT (appsrc), "caps",
+                gst_caps_new_simple ("video/x-raw",
+                                     "format", G_TYPE_STRING, "RGB16",
+                                     "width", G_TYPE_INT, 384,
+                                     "height", G_TYPE_INT, 288,
+                                     "framerate", GST_TYPE_FRACTION, 0, 1, NULL), NULL);
+
+  ctx = g_new0 (MyContext, 1);
+  ctx->white = FALSE;
+  ctx->timestamp = 0;
+  /* make sure ther datais freed when the media is gone */
+  g_object_set_data_full (G_OBJECT (media), "my-extra-data", ctx,
+                          (GDestroyNotify) g_free);
+
+  /* install the callback that will be called when a buffer is needed */
+  g_signal_connect (appsrc, "need-data", (GCallback) need_data, ctx);
+  gst_object_unref (appsrc);
+  gst_object_unref (element);
+}
+
 /* Main method for the native code. This is executed on its own thread. */
 static void *app_function (void *userdata) {
-  JavaVMAttachArgs args;
-  GstBus *bus;
-  CustomData *data = (CustomData *)userdata;
-  GSource *bus_source;
-  GError *error = NULL;
+  GMainLoop *loop;
+  GstRTSPServer *server;
+  GstRTSPMountPoints *mounts;
+  GstRTSPMediaFactory *factory;
 
-  GST_DEBUG ("Creating pipeline in CustomData at %p", data);
+//  gst_init (&argc, &argv);
 
-  /* Create our own GLib Main Context and make it the default one */
-  data->context = g_main_context_new ();
-  g_main_context_push_thread_default(data->context);
+  loop = g_main_loop_new (NULL, FALSE);
 
-  /* Build pipeline */
-  data->pipeline = gst_parse_launch("audiotestsrc ! audioconvert ! audioresample ! autoaudiosink", &error);
-  if (error) {
-    gchar *message = g_strdup_printf("Unable to build pipeline: %s", error->message);
-    g_clear_error (&error);
-    set_ui_message(message, data);
-    g_free (message);
-    return NULL;
-  }
+  /* create a server instance */
+  server = gst_rtsp_server_new ();
 
-  /* Instruct the bus to emit signals for each received message, and connect to the interesting signals */
-  bus = gst_element_get_bus (data->pipeline);
-  bus_source = gst_bus_create_watch (bus);
-  g_source_set_callback (bus_source, (GSourceFunc) gst_bus_async_signal_func, NULL, NULL);
-  g_source_attach (bus_source, data->context);
-  g_source_unref (bus_source);
-  g_signal_connect (G_OBJECT (bus), "message::error", (GCallback)error_cb, data);
-  g_signal_connect (G_OBJECT (bus), "message::state-changed", (GCallback)state_changed_cb, data);
-  gst_object_unref (bus);
+  /* get the mount points for this server, every server has a default object
+   * that be used to map uri mount points to media factories */
+  mounts = gst_rtsp_server_get_mount_points (server);
 
-  /* Create a GLib Main Loop and set it to run */
-  GST_DEBUG ("Entering main loop... (CustomData:%p)", data);
-  data->main_loop = g_main_loop_new (data->context, FALSE);
-  check_initialization_complete (data);
-  g_main_loop_run (data->main_loop);
-  GST_DEBUG ("Exited main loop");
-  g_main_loop_unref (data->main_loop);
-  data->main_loop = NULL;
+  /* make a media factory for a test stream. The default media factory can use
+   * gst-launch syntax to create pipelines.
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory = gst_rtsp_media_factory_new ();
+  gst_rtsp_media_factory_set_launch (factory,
+                                     "( videotestsrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 )");
 
-  /* Free resources */
-  g_main_context_pop_thread_default(data->context);
-  g_main_context_unref (data->context);
-  gst_element_set_state (data->pipeline, GST_STATE_NULL);
-  gst_object_unref (data->pipeline);
+  /* notify when our media is ready, This is called whenever someone asks for
+   * the media and a new pipeline with our appsrc is created */
+//  g_signal_connect (factory, "media-configure", (GCallback) media_configure,
+//                    NULL);
+
+  /* attach the test factory to the /test url */
+  gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
+
+  /* don't need the ref to the mounts anymore */
+  g_object_unref (mounts);
+
+  /* attach the server to the default maincontext */
+  gst_rtsp_server_attach (server, NULL);
+
+  /* start serving */
+  g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
+  g_main_loop_run (loop);
 
   return NULL;
+
+//  JavaVMAttachArgs args;
+//  GstBus *bus;
+//  CustomData *data = (CustomData *)userdata;
+//  GSource *bus_source;
+//  GError *error = NULL;
+//
+//  GST_DEBUG ("Creating pipeline in CustomData at %p", data);
+//
+//  /* Create our own GLib Main Context and make it the default one */
+//  data->context = g_main_context_new ();
+//  g_main_context_push_thread_default(data->context);
+//
+//  /* Build pipeline */
+//  data->pipeline = gst_parse_launch("audiotestsrc ! audioconvert ! audioresample ! autoaudiosink", &error);
+//  if (error) {
+//    gchar *message = g_strdup_printf("Unable to build pipeline: %s", error->message);
+//    g_clear_error (&error);
+//    set_ui_message(message, data);
+//    g_free (message);
+//    return NULL;
+//  }
+//
+//  /* Instruct the bus to emit signals for each received message, and connect to the interesting signals */
+//  bus = gst_element_get_bus (data->pipeline);
+//  bus_source = gst_bus_create_watch (bus);
+//  g_source_set_callback (bus_source, (GSourceFunc) gst_bus_async_signal_func, NULL, NULL);
+//  g_source_attach (bus_source, data->context);
+//  g_source_unref (bus_source);
+//  g_signal_connect (G_OBJECT (bus), "message::error", (GCallback)error_cb, data);
+//  g_signal_connect (G_OBJECT (bus), "message::state-changed", (GCallback)state_changed_cb, data);
+//  gst_object_unref (bus);
+//
+//  /* Create a GLib Main Loop and set it to run */
+//  GST_DEBUG ("Entering main loop... (CustomData:%p)", data);
+//  data->main_loop = g_main_loop_new (data->context, FALSE);
+//  check_initialization_complete (data);
+//  g_main_loop_run (data->main_loop);
+//  GST_DEBUG ("Exited main loop");
+//  g_main_loop_unref (data->main_loop);
+//  data->main_loop = NULL;
+//
+//  /* Free resources */
+//  g_main_context_pop_thread_default(data->context);
+//  g_main_context_unref (data->context);
+//  gst_element_set_state (data->pipeline, GST_STATE_NULL);
+//  gst_object_unref (data->pipeline);
+//
+//  return NULL;
 }
 
 /*
